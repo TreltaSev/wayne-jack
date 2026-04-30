11a8d54b0b422e15df09f424c66beab8a96ee77d
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2026-04-30 12:06:09 -0400

    feat: setup level_1_dialog

--- Assets/Dialogue/TextLink.cs
+++ Assets/Dialogue/TextLink.cs
@@ -1,19 +1,59 @@
+using NUnit.Framework.Constraints;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Mode dropdown selection
+/// </summary>
+public enum Mode
+{
+    Transmitter,
+    Receiver
+}
+
 
 public class TextLink : MonoBehaviour
 {
 
-    
+    // === CONTENT === //
+    [Tooltip("Content of the text box")]
+    private string content;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    // === MODE === //
+    [Tooltip("Sets the mode of the text link")]
+    private Mode mode;
+
+    // --- Events ---
+    private UnityEvent<string> e_content_change;
+
+    public void SetContent(string new_content)
     {
-        
+        this.content = new_content;
+        this.e_content_change.Invoke(new_content);
     }
 
-    // Update is called once per frame
-    void Update()
+    [CustomEditor(typeof(TextLink))]
+    public class TextLinkEditor : Editor
     {
-        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            TextLink textLink = (TextLink)target;
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Mode");
+            textLink.mode = (Mode)EditorGUILayout.EnumPopup("Mode", textLink.mode);
+            EditorGUILayout.BeginHorizontal();
+
+            
+            GameObject transmitter = EditorGUILayout.ObjectField("Some Field", null, typeof(GameObject), true) as GameObject;
+
+            EditorGUILayout.EndHorizontal();
+
+
+        }
     }
 }
