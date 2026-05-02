c3c5f2938d78994d2cac63eced803db003b252c3
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2026-05-02 11:08:23 -0400

    feat: Added text link component & prefab

--- Assets/Dialogue/TextLink.cs
+++ Assets/Dialogue/TextLink.cs
@@ -1,11 +1,8 @@
-using NUnit.Framework.Constraints;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
-/// <summary>
-/// Mode dropdown selection
-/// </summary>
 public enum Mode
 {
     Transmitter,
@@ -13,47 +10,157 @@ public enum Mode
 }
 
 
+[ExecuteAlways]
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class TextLink : MonoBehaviour
 {
 
     // === CONTENT === //
-    [Tooltip("Content of the text box")]
+    [SerializeField, Tooltip("Content of the text box")]
     private string content;
-
+    private string previous_content = "";
 
     // === MODE === //
-    [Tooltip("Sets the mode of the text link")]
+    [SerializeField, Tooltip("Sets the mode of the text link")]
     private Mode mode;
 
-    // --- Events ---
-    private UnityEvent<string> e_content_change;
+    // === LISTEN TO === //
+    [SerializeField] private TextLink listenTo;
+    private TextLink currentlyListeningTo;
 
+    // === TEXT COMPONENT === //
+    private TextMeshProUGUI textComponent;
+
+    // --- Events ---
+    private readonly UnityEvent<string> e_content_change = new();
+
+    /// <summary>
+    /// Updates the content value programmatically.
+    /// Also calls the content change event.
+    /// </summary>
+    /// <param name="new_content"></param>
     public void SetContent(string new_content)
     {
+        content = new_content;
+        ApplyContentIfChanged();
+    }
+
+    /// <summary>
+    /// Gets the textComponent reference if there isn't one already
+    /// </summary>
+    private void Cache()
+    {
+        if (!textComponent)
+            textComponent = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnValidate()
+    {
+        Cache();
+        SetupListener();
+        ApplyContentIfChanged();
+    }
+
+    private void OnEnable()
+    {
+        Cache();
+        SetupListener();
+        ApplyContentIfChanged();
+    }
+
+    /// <summary>
+    /// Sets up the listener if this component is in receiver mode
+    /// </summary>
+    private void SetupListener()
+    {
+
+        Debug.Log("First");
+
+        if (mode != Mode.Receiver || listenTo == null) return;
+
+        if (currentlyListeningTo != null)
+            currentlyListeningTo.e_content_change.RemoveListener(TransmitterListener);
+        Debug.Log("Fourth");
+
+        currentlyListeningTo = listenTo;
+
+        Debug.Log("Added Listener");
+        currentlyListeningTo.e_content_change.AddListener(TransmitterListener);
+    }
+
+    /// <summary>
+    /// Checks the current and previous saved contents and invokes the event if its a transmitter.
+    /// </summary>
+    private void ApplyContentIfChanged()
+    {
+        if (previous_content == content) return;
+
+        previous_content = content;
+
+        if (textComponent)
+            textComponent.text = content;
+
+        if (mode == Mode.Transmitter)
+            e_content_change.Invoke(content);
+    }
+
+    public void TransmitterListener(string new_content)
+    {
+        Debug.Log($"Transmit {this} {new_content}");
         this.content = new_content;
-        this.e_content_change.Invoke(new_content);
+        this.textComponent.text = this.content;
     }
 
     [CustomEditor(typeof(TextLink))]
     public class TextLinkEditor : Editor
     {
+        private TextLink textLink;
+
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
+            this.textLink = (TextLink)target;
 
-            TextLink textLink = (TextLink)target;
+            // Display Editor Options
+            this.DisplayEditor();
+        }
+
+        private void DisplayEditor()
+        {
+
+            EditorGUI.BeginChangeCheck();
 
             EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Mode");
-            textLink.mode = (Mode)EditorGUILayout.EnumPopup("Mode", textLink.mode);
-            EditorGUILayout.BeginHorizontal();
+
+            // Mode
+            this.textLink.mode = (Mode)EditorGUILayout.EnumPopup("Mode", textLink.mode);
+            EditorGUILayout.Space();
+
+            // Content ( Dependant on Mode )
+            if (this.textLink.mode == Mode.Transmitter)
+            {
+                EditorGUILayout.LabelField("Content");
+                this.textLink.content = EditorGUILayout.TextField(this.textLink.content);
+                EditorGUILayout.Space();
+            }
+
+            // Transmitter ( Depends on Mode == Mode.Receiver )
+            if (textLink.mode == Mode.Receiver)
+            {
+                textLink.listenTo = (TextLink)EditorGUILayout.ObjectField("Listen To", this.textLink.listenTo, typeof(TextLink), true);
+                EditorGUILayout.Space();
+            }
 
             
-            GameObject transmitter = EditorGUILayout.ObjectField("Some Field", null, typeof(GameObject), true) as GameObject;
 
-            EditorGUILayout.EndHorizontal();
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(textLink, "TextLink Change");
 
+                textLink.Cache();
+                textLink.ApplyContentIfChanged();
 
+                EditorUtility.SetDirty(textLink);
+            }
         }
     }
 }
